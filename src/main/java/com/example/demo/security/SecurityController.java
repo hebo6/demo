aa09abe0e6ffc6c36b5f35102f262fab25bfb084
Bo He <qq111372757@gmail.com>
2021-03-02 20:46:01 +0800

    improve security test

--- src/main/java/com/example/demo/security/SecurityController.java
+++ src/main/java/com/example/demo/security/SecurityController.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import com.example.demo.jpa.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class SecurityController {
             answer = "Hello, " + name + ". " + answer;
         }
         return answer;
+    }
+
+    @PostMapping
+    public String addResources() {
+        return "Success";
     }
 }
--- src/test/java/com/example/demo/security/SecurityControllerTest.java
+++ src/test/java/com/example/demo/security/SecurityControllerTest.java
@@ -5,19 +5,23 @@ import com.example.demo.jpa.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(value = SecurityController.class)
-@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(
+        value = SecurityController.class,
+        properties = {"spring.security.user.name=123", "spring.security.user.password=456"}
+)
 class SecurityControllerTest {
     @Autowired
     MockMvc mockMvc;
@@ -33,8 +37,37 @@ class SecurityControllerTest {
     }
 
     @Test
-    void findResources() throws Exception {
-        this.mockMvc.perform(get("/security")).andDo(print()).andExpect(status().isOk())
+    void findResourcesWithoutUser() throws Exception {
+        this.mockMvc.perform(get("/security")).andDo(print()).andExpect(status().isFound());
+    }
+
+    @Test
+    void findResourcesWithUser() throws Exception {
+        this.mockMvc.perform(
+                get("/security")
+                        .with(user("123").password("456"))
+        ).andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(content().string("Hello, " + mockName + ". You are now accessing secure resources"));
+    }
+
+    @Test
+    void addResourcesWithoutCsrf() throws Exception {
+        this.mockMvc.perform(
+                post("/security")
+                        .with(user("123").password("456"))
+        ).andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void addResourcesWithCsrf() throws Exception {
+        this.mockMvc.perform(
+                post("/security")
+                        .with(user("123").password("456"))
+                        .with(csrf())
+        ).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Success"));
     }
 }
