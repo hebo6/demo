8b85014eeb035d6b1fbc6dbf305b64f2cb36bfd1
Bo He <qq111372757@gmail.com>
2021-03-03 16:37:55 +0800

    Update DemoProperties.java

--- src/main/java/com/example/demo/properties/DemoProperties.java
+++ src/main/java/com/example/demo/properties/DemoProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "demo")
+@ConfigurationProperties("demo")
 @Data
 public class DemoProperties {
     private String name;
