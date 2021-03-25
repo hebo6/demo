925a4e8bbb32b9ea17125539df939aad243ec7dd
Bo He <qq111372757@gmail.com>
2021-03-25 10:09:29 +0800

    Update GlobalExceptionHandler.java

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,13 +1,16 @@
 package com.example.demo.exception;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public Error handleException(Exception e) {
+        log.error("捕获到异常", e);
         Error error = new Error();
         error.setCode(500);
         error.setMsg(e.getMessage());
