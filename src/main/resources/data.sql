b7c2bff477edcbd63ee219d73baf38b7ad8cbb50
Bo He <qq111372757@gmail.com>
2021-03-27 22:40:01 +0800

    init data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,9 @@
 insert into address (`id`, `city`, `country`)
-values (1, 'beijing', 'china');
+values (1, 'beijing', 'china'),
+       (2, 'shenzhen', 'china');
+
+insert into user (`id`, `name`, `age`)
+values (1, 'haha', 18);
+
+insert into user_address (`user_id`, `address_id`)
+values (1, 1);
