f2e815db23375f933c177a15f41aca207969b440
Bo He <qq111372757@gmail.com>
2021-07-05 23:05:08 +0800

    add validation

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,16 +1,65 @@
 package com.example.demo.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    /**
+     * <1> 处理 form data方式调用接口校验失败抛出的异常
+     */
+    @ExceptionHandler(BindException.class)
+    public Error bindExceptionHandler(BindException e) {
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        List<String> collect = fieldErrors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        return new Error(HttpStatus.BAD_REQUEST.value(), collect.toString());
+    }
+
+    /**
+     * <2> 处理 json 请求体调用接口校验失败抛出的异常
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Error methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        List<String> collect = fieldErrors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        return new Error(HttpStatus.BAD_REQUEST.value(), collect.toString());
+    }
+
+    /**
+     * <3> 处理单个参数校验失败抛出的异常
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public Error constraintViolationExceptionHandler(ConstraintViolationException e) {
+        Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
+        List<String> collect = constraintViolations.stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.toList());
+        return new Error(HttpStatus.BAD_REQUEST.value(), collect.toString());
+    }
+
     @ExceptionHandler(Exception.class)
     public Error handleException(Exception e) {
-        log.error("捕获到异常", e);
+        log.error("捕获到其它异常", e);
         Error error = new Error();
         error.setCode(500);
         error.setMsg(e.getMessage());
@@ -18,6 +67,8 @@ public class GlobalExceptionHandler {
     }
 
     @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
     private static class Error {
         private Integer code;
         private String msg;
