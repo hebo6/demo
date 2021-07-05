e0d75e217898009e0916dca71fbc1d43a9212e23
Bo He <qq111372757@gmail.com>
2021-07-05 22:01:21 +0800

    add DownloadUtils.java

---
+++ src/main/java/com/example/demo/http/DownloadController.java
@@ -0,0 +1,21 @@
+package com.example.demo.http;
+
+import com.example.demo.utils.DownloadUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@RestController
+@RequestMapping("download")
+public class DownloadController {
+    @GetMapping
+    public void downloadChineseFileName(HttpServletResponse response) throws IOException {
+        String fileName = "我是中文名.txt";
+        byte[] bytes = "hello，我是内容！啊啊啊".getBytes(StandardCharsets.UTF_8);
+        DownloadUtils.download(response, fileName, bytes);
+    }
+}
---
+++ src/main/java/com/example/demo/utils/DownloadUtils.java
@@ -0,0 +1,19 @@
+package com.example.demo.utils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class DownloadUtils {
+    private DownloadUtils() {
+    }
+
+    /**
+     * 下载文件能正常显示中文
+     */
+    public static void download(HttpServletResponse response, String fileName, byte[] bytes) throws IOException {
+        response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, StandardCharsets.UTF_8));
+        response.getOutputStream().write(bytes);
+    }
+}
