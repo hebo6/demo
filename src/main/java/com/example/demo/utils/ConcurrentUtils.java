a0669bad636e395f757035821285a5d5f9d05492
Bo He <qq111372757@gmail.com>
2021-09-29 17:50:41 +0800

    Update ConcurrentUtils.java

--- src/main/java/com/example/demo/utils/ConcurrentUtils.java
+++ src/main/java/com/example/demo/utils/ConcurrentUtils.java
@@ -3,20 +3,60 @@ package com.example.demo.utils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ConcurrentUtils {
+    private static final Map<Object, Lock> obj2Lock = new ConcurrentHashMap<>();
 
-    public static <T> T synchronize(Supplier<T> supplier, Lock lock) {
-        if (lock.tryLock()) {
-            try {
-                return supplier.get();
-            } finally {
-                lock.unlock();
-            }
+    /**
+     * @param supplier 要执行的函数
+     */
+    public static <T> T synchronize(Supplier<T> supplier, Object obj) {
+        Lock lock = lock(obj);
+
+        try {
+            return supplier.get();
+        } finally {
+            lock.unlock();
         }
-        return null;
+    }
+
+    /**
+     * @param processor 要执行的函数
+     */
+    public static void synchronize(Processor processor, Object obj) {
+        Lock lock = lock(obj);
+
+        try {
+            processor.run();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * @param obj 与锁对应的对象
+     * @throws RuntimeException 如果获取锁失败
+     */
+    private static Lock lock(Object obj) {
+        Lock lock = obj2Lock.get(obj);
+        if (lock == null) {
+            synchronized (obj2Lock) {
+                if (obj2Lock.get(obj) == null) {
+                    obj2Lock.put(obj, new ReentrantLock());
+                }
+            }
+            lock = obj2Lock.get(obj);
+        }
+
+        if (!lock.tryLock()) {
+            throw new RuntimeException("任务已在执行中");
+        }
+        return lock;
     }
 }
