0c37e30292d966be0000541777087598ca81a26e
Bo He <qq111372757@gmail.com>
2021-12-13 10:56:47 +0800

    update

--- src/main/java/com/example/demo/utils/DownloadUtils.java
+++ src/main/java/com/example/demo/utils/DownloadUtils.java
@@ -10,14 +10,22 @@ import java.nio.charset.StandardCharsets;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DownloadUtils {
+    private final static String filenamePrefix = "UTF-8''";
 
     /**
      * 下载文件能正常显示中文
      */
-    public static void download(byte[] bytes, String fileName, HttpServletResponse response) throws IOException {
-        response.setHeader("Content-Disposition",
-                "attachment; filename=" + URLEncoder.encode(fileName, StandardCharsets.UTF_8)
-                        + "; filename*=UTF-8''" + URLEncoder.encode(fileName, StandardCharsets.UTF_8));
+    public static void download(byte[] bytes, String filename, HttpServletResponse response) throws IOException {
+        setDownloadHeader(response, filename);
         response.getOutputStream().write(bytes);
+    }
+
+    public static void setDownloadHeader(HttpServletResponse response, String filename) {
+        if (filename.startsWith(filenamePrefix)) {
+            filename = filename.substring(filenamePrefix.length());
+        }
+        //URLEncoder: 防止中文文件名乱码
+        response.setHeader("Content-Disposition", "attachment; filename="
+                + URLEncoder.encode(filename, StandardCharsets.UTF_8) + "; filename*=" + filenamePrefix + URLEncoder.encode(filename, StandardCharsets.UTF_8));
     }
 }
