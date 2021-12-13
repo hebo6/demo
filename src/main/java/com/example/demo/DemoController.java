e01ecab6db18087a28d12ba82c27d0e42ba7d4ef
Bo He <qq111372757@gmail.com>
2021-12-13 11:08:34 +0800

    restore to init state

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping
     public String demo() {
-        return "Hello Demo.";
+        return "hello world";
     }
 }
--- src/main/java/com/example/demo/jpa/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.jpa;
-
-import com.example.demo.jpa.entity.User;
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-public interface UserRepository extends JpaRepositoryImplementation<User, Long> {
-}
